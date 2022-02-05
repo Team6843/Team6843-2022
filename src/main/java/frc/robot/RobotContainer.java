95a7379178419b6b0e2bdece686c698dfa13f66a
cgcianciolo <cameron.cianciolo.2023@smacatholic.org>
2022-02-05 14:14:04 -0600

    First commit of Swerve code

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.commands.SwerveDriveCommand;
+import frc.robot.subsystems.SwerveDriveTrain;
+
+/** The structure of the robot (including subsystems, commands, and button mappings) should be declared here */
+public class RobotContainer 
+{
+    private final XboxController controller = new XboxController(0);
+
+    private final SwerveDriveTrain driveTrain = new SwerveDriveTrain();
+
+    /** The container for the robot. Contains subsystems, OI devices, and commands. */
+    public RobotContainer() 
+    {
+        driveTrain.setDefaultCommand(new SwerveDriveCommand(driveTrain, controller));
+    }
+}
